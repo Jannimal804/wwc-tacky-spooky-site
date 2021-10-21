2cee7410ea94ee409e2eca76ba3223cb37c0d08a
Aubrey Northam <aubrey.northam@mobelux.com>
2021-10-21 18:47:26 -0400

    Getting spooky in the browser console

---
+++ javascript/consoleSauce.js
@@ -0,0 +1,12 @@
+// console sauce
+const style = "font-size: 40px;" +
+  "background: #324759;" +
+  "background: -webkit-linear-gradient(to right, #282622, #941313, #282622);" +
+  "background: linear-gradient(to right, #282622, #941313, #282622);" +
+  "color: white;" +
+  "text-align: center;" +
+  "padding: 10px 15px;" +
+  "width: 100%;" +
+  "border-radius: 32px;";
+const text = "%c👻👻BOO!👻👻";
+console.log(text, style);
